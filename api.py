7a9f52b51a383e63104ca80a76c7581112e45080
Jake Romer <jkrmr.io@gmail.com>
2017-05-21 12:27:12 -0400

    Add content recommender get and put routes
    Introduces ContentRecommender, which exposes methods to train a model and
    perform predictions.
    
    For now:
    - Trains model based on CSV file packaged with the application
    - Uses a hard-coded REDIS_URL

--- api.py
+++ api.py
@@ -1,12 +1,32 @@
 from flask import Flask
-from flask_restful import Api
+from flask_restful import Api, Resource, reqparse
 
-from content_recommender import Recommendation
+from content_recommender import Recommender
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Recommendation, "/")
+
+class Recommendation(Resource):
+    def put(self):
+        recommender = Recommender()
+        recommender.train("./data/squawks.csv")
+        return {"success": True}
+
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument("squawk_id", type=int, required=True)
+        args = parser.parse_args()
+        item_id = args['squawk_id']
+        num_recommendations = 5
+
+        recommender = Recommender()
+        prediction = recommender.predict(item_id, num_recommendations)
+        ids = list(map(lambda b: int(b.decode("utf-8")), prediction))
+        return {"squawk_ids": ids}
+
+
+api.add_resource(Recommendation, "/recommendation")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- content_recommender.py
+++ content_recommender.py
@@ -1,15 +1,44 @@
 import time
 
-from flask_restful import Resource
+import pandas as pd
+import redis
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 
-# import numpy as np
-# import pandas as pd
-# import redis
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.metrics_pairwise import linear_kernel
+class Recommender(object):
+    SIMKEY = "p:smlr:%s"
+    REDIS_URL = "redis://localhost:6379"
 
+    def __init__(self):
+        self._redis = redis.StrictRedis.from_url(self.REDIS_URL)
 
-class Recommendation(Resource):
-    def get(self):
-        return {"hello": "dave"}
+    def train(self, csv_path):
+        start = time.time()
+        df = pd.read_csv(csv_path)
+        print("Loading data: %s secs." % (time.time() - start))
+        start = time.time()
+        self._train_engine(df)
+        print("Training model: %s secs." % (time.time() - start))
+
+    def predict(self, item_id, num):
+        return self._redis.zrange(
+            self.SIMKEY % item_id, 0, num - 1, withscores=False, desc=True)
+
+    def _train_engine(self, data_frame):
+        tf = TfidfVectorizer(
+            analyzer="word",
+            ngram_range=(1, 3),
+            min_df=0,
+            stop_words="english")
+
+        content = data_frame["content"]
+        tfidf_matrix = tf.fit_transform(content)
+        cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+        for idx, row in data_frame.iterrows():
+            similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+            similar_items = [(cosine_similarities[idx][i], data_frame["id"][i])
+                             for i in similar_indices]
+            flattened = sum(similar_items[1:], ())
+            self._redis.zadd(self.SIMKEY % row["id"], *flattened)
